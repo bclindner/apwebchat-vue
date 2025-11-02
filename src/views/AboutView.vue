fa2deaa22d1aa663401d7fb81fbc8b2e85760297
Brian Lindner <brian@bclindner.com>
2025-11-02 16:56:40 -0500

    whoops, fixed license on page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,7 +3,7 @@
     <h1>APWebChat</h1>
     <p>
       Developed by <a href="https://bclindner.com">bclindner</a> 2024 under
-      <a href="https://mit-license.org/">MIT license</a>.
+      <a href="https://www.gnu.org/licenses/gpl-3.0.en.html">GPL 3.0 license</a>.
     </p>
     <p>
       Archipelago logo &copy; 2022 Krista Corkos and Christopher Wilson, licensed under
