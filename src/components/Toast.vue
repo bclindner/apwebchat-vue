924e7442fecaf97a69ab522d19b68da8db2fe18e
Brian Lindner <brian@bclindner.com>
2025-11-17 17:56:33 -0500

    fix: forgot to add the toast, whoops

---
+++ src/components/Toast.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+  defineProps<{
+    title: string,
+    text: string,
+    duration: number,
+  }>()
+  defineEmits<{
+    (e: 'close'): void
+  }>()
+</script>
+
+<template>
+  <div class="toast">
+    <div class="toast-content">
+      <h2>{{title}}</h2>
+      <p>{{text}}</p>
+    </div>
+    <button class="exit-button" @click="$emit('close')">X</button>
+  </div>
+</template>
+
+<style scoped>
+.toast {
+  position: fixed;
+  bottom: 8px;
+  max-width: 700px;
+  width: 100%;
+  left: 50%;
+  transform: translate(-50%, 0);
+  border-radius: 4px;
+  padding: 0;
+  margin: 0;
+  background-color: #ff0000e0;
+}
+
+.toast-content {
+  padding: 1em;
+}
+
+.exit-button {
+  position: absolute;
+  right: 1em;
+  top: 1em;
+  background: white;
+  color: black;
+  border: none;
+  padding: 0.25em;
+  margin: 0;
+  border-radius: 4px;
+}
+</style>
